f67f05abb14650478acf4c09f0f0ac405e579514
Zeke Sikelianos <zeke@sikelianos.com>
2017-03-29 14:29:06 -0700

    start it

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ummoji Update Server
+
+Squirrel update server for the Ummoji desktop app.
+
+## Promoting a Release
+
+```
+heroku config:set UMMOJI_STABLE_VERSION=1.1.0
+```
+
+## Downloading Releases
+
+The following convenience URLs redirect to the latest release for a given platform:
+
+- [updates/ummoji-linux.zip](https://ummoji.herokuapp.com/updates/ummoji-linux.zip)
+- [updates/ummoji-mac.zip](https://ummoji.herokuapp.com/updates/ummoji-mac.zip)
+- [updates/ummoji-windows.zip](https://ummoji.herokuapp.com/updates/ummoji-windows.zip)
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+const url = process.env.URL || 'https://ummoji.herokuapp.com'
+const port = Number(process.env.PORT) || 5000
+const downloadUrl = 'https://github.com/electron/ummoji/releases/download'
+
+app.set('port', port)
+
+app.listen(app.get('port'), () => {
+  console.log('Server is running on port', app.get('port'))
+})
+
+app.get('/', (request, response) => {
+  response.redirect('https://github.com/ummoji/ummoji-update-server')
+})
+
+app.get('/updates/:asset', (request, response) => {
+  const asset = request.params.asset
+  const version = process.env.UMMOJI_STABLE_VERSION
+  response.redirect(`${downloadUrl}/v${version}/${asset}`)
+})
+
+app.get('/updates', (request, response) => {
+  const version = request.query.version
+  const latestRelease = process.env.UMMOJI_STABLE_VERSION
+  if (version === latestRelease) {
+    response.status(204).end()
+  } else {
+    response.json({
+      name: `Ummoji v${latestRelease}`,
+      notes: 'The latest release of the app',
+      pub_date: new Date().toISOString(),
+      url: `${url}/updates/ummoji-mac.zip`
+    })
+  }
+})
